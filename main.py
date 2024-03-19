874159587cc56cd2ad653dc782b0c29e6387c7d0
ebert-sl <ebertis2002@gmail.com>
2024-03-19 13:04:26 -0300

    fix: correção na análise de urls e ordenar xlsx
    Antes apenas as tags <a> eram verificadas para pontuação, agora
    o link base está devidamente analisado.
    
    Antes a lista de links salvos era resetada em cada chamada da
    função main, agora a lista está externa a função main.
    
    Criada a função process_url para evitar repetição de código.
    
    Aplicada a ordenação dos dados no arquivo xlsx com base no total
    de pontos.

--- main.py
+++ main.py
@@ -16,11 +16,36 @@ from funcoes_pontuacao import (
     uso_em_tags,
 )
 
+links_visitados = set()
+
+def process_url(href, soup, termo, writer):    
+    print()
+    print("-----------------------")
+    print(f"\nPontuando a página: {href}")
+    print("\n-----------------------")
+
+    autoridade_val = autoridade(soup)
+    frequencia_termos_val = frequencia_termos(soup, termo)
+    uso_em_tags_val = uso_em_tags(soup, termo)
+    auto_referencia_val = auto_referencia(soup, href)
+    frescor_val = frescor(soup)
+
+    total = (
+        autoridade_val
+        + frequencia_termos_val
+        + uso_em_tags_val
+        + auto_referencia_val
+        + frescor_val
+    )
+
+    print(f"Total: {total} pontos")
+    print("-----------------------\n")
+    writer.writerow([href, autoridade_val, frequencia_termos_val, uso_em_tags_val, auto_referencia_val, frescor_val, total])
+
 def main():
     url = input("Digite a URL desejada: ")
     termo = input("Digite o termo que deseja buscar: ")
-    links_visitados = set()
-
+    
     if re.match(r"^\s*$", termo):
         # ^ início da string | \s* zero ou mais espaços em branco | $ final da string
         print("O termo está vazio ou contém apenas espaços em branco.\n")
@@ -31,11 +56,15 @@ def main():
         response = requests.get(url, verify=True)
         soup = BeautifulSoup(response.text, "html.parser")
 
-        with open('data.csv', 'w', newline='') as file:
+        with open('data.csv', 'a', newline='') as file:
             writer = csv.writer(file)
             if not os.path.exists('data.csv') or os.stat('data.csv').st_size == 0:
                 writer.writerow(["URL", "Autoridade", "Frequencia Termos", "Uso em Tags", "Auto Referencia", "Frescor", "Total"])
-        
+
+            if url not in links_visitados:
+                links_visitados.add(url)
+                process_url(url, soup, termo, writer)
+
             for link in soup.find_all("a"):
                 href = link.get("href")
                 if href:
@@ -45,29 +74,7 @@ def main():
                         links_visitados.add(href)
                         link_response = requests.get(href, verify=True)
                         link_soup = BeautifulSoup(link_response.text, "html.parser")
-                        
-                        print()
-                        print("-----------------------")
-                        print(f"\nPontuando a página: {href}")
-                        print("\n-----------------------")
-
-                        autoridade_val = autoridade(link_soup)
-                        frequencia_termos_val = frequencia_termos(link_soup, termo)
-                        uso_em_tags_val = uso_em_tags(link_soup, termo)
-                        auto_referencia_val = auto_referencia(link_soup, url)
-                        frescor_val = frescor(link_soup)
-
-                        total = (
-                            autoridade_val
-                            + frequencia_termos_val
-                            + uso_em_tags_val
-                            + auto_referencia_val
-                            + frescor_val
-                        )
-
-                        print(f"Total: {total} pontos")
-                        print("-----------------------\n")
-                        writer.writerow([href, autoridade_val, frequencia_termos_val, uso_em_tags_val, auto_referencia_val, frescor_val, total])
+                        process_url(href, link_soup, termo, writer)
 
     except Exception:
         print(
@@ -78,9 +85,10 @@ def main():
 
     menu_continuar()
 
-def csv_to_excel():
+def csv_para_excel():
     data = pd.read_csv('data.csv')
-    data.to_excel('data.xlsx', index=False)
+    data_ordenada = data.sort_values('Total', ascending=False)
+    data_ordenada.to_excel('data_ordenada.xlsx', index=False)
 
 def menu_continuar():
     continuar = input("Deseja continuar usando o programa?\n1 - Sim\n2 - Não\n")
@@ -89,7 +97,7 @@ def menu_continuar():
         main()
     elif continuar == "2":
         print("\nAté logo!")
-        csv_to_excel()
+        csv_para_excel()
         sys.exit()
     else:
         print("-----------------\nComando inválido!\n-----------------\n")
